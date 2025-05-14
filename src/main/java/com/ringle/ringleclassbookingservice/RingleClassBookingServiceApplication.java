6753d09534dedde81e5642803a9e0438b42d301a
정태승 <hi563@g.skku.edu>
2025-05-14 22:54:07 +0900

    init: 최초 설정

---
+++ src/main/java/com/ringle/ringleclassbookingservice/RingleClassBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ringle.ringleclassbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingleClassBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RingleClassBookingServiceApplication.class, args);
+    }
+
+}
